20553adbe32099913e7c76ec1b2fe04cabc071cf
zm <735583465@qq.com>
2020-08-19 00:46:29 +0800

    xiugai

--- js学习/js语法/js内置对象练习/tool.js
+++ js学习/js语法/js内置对象练习/tool.js
@@ -43,3 +43,74 @@ function randomOpcityColor() {
         return str;
     }
 }
+
+
+// 鼠标拖拽移动物体,可以拖拽出窗口界的
+function drag(node) {
+    node.onmousedown = function(ev) {
+        var e = ev || window.event;
+        var offsetX = e.clientX - node.offsetLeft;
+        var offsetY = e.clientY - node.offsetTop;
+        document.onmousemove = function(ev) {
+            var e = ev || window.event;
+            node.style.left = e.clientX - offsetX + 'px';
+            node.style.top = e.clientY - offsetY + 'px';
+        }
+    }
+    document.onmouseup = function() {
+        document.onmousemove = null;
+    }
+}
+
+// 鼠标拖拽移动物体,不能出窗口界的
+function limitDrag(node) {
+    node.onmousedown = function(ev) {
+        var e = ev || window.event;
+        var offsetX = e.clientX - node.offsetLeft;
+        var offsetY = e.clientY - node.offsetTop;
+        document.onmousemove = function(ev) {
+            var e = ev || window.event;
+            var l = e.clientX - offsetX;
+            var t = e.clientY - offsetY;
+
+            if (l <= 0) {
+                l = 0;
+            }
+            var windowWidth = document.documentElement.clientWidth || document.body.clientWidth;
+            if (l >=
+                windowWidth - node.offsetWidth) {
+                l = windowWidth - node.offsetWidth;
+            }
+            if (t <= 0) {
+                t = 0;
+            }
+            var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+            if (t >= windowHeight - node.offsetHeight) {
+                t = windowHeight - node.offsetHeight;
+            }
+
+            node.style.left = l + 'px';
+            node.style.top = t + 'px';
+        }
+    }
+    document.onmouseup = function() {
+        document.onmousemove = null;
+    }
+}
+
+// 事件监听兼容写法
+function addEvent(node, eventType, funcName) {
+    if (node.addEventListener) {
+        node.addEventListener(eventType, funcName, false);
+    } else {
+        node.attachEvent('on' + eventType, funcName);
+    }
+}
+// 删除事件监听器兼容写法
+function removeEvent(node, eventType, funcName) {
+    if (node.removeEventListener) {
+        node.removeEventListener(eventType, funcName);
+    } else {
+        node.detachEvent('on' + eventType, funcName);
+    }
+}
